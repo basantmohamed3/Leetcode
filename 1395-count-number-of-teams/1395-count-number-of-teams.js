7a3616340b5a19852e4a24c33227ffd98858f5d0
Basant Mohamed <39272090+basantmohamed3@users.noreply.github.com>
2022-07-02 19:36:46 +0200

    Time: 5407 ms (10.58%), Space: 41.9 MB (83.65%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} rating
+ * @return {number}
+ */
+var numTeams = function(rating) {
+      let cnt = 0;
+for (let i =0; i<rating.length; i++){
+  for (let j= i+1; j <rating.length;j++){
+    for(let k = j+1; k< rating.length; k++){
+      if((rating[i]> rating[j] &&rating[j]> rating[k]) ||(rating[i]< rating[j] &&rating[j]<rating[k]) ){
+
+        cnt = cnt+1
+      }
+
+    }
+  }
+}
+return cnt
+    
+};
