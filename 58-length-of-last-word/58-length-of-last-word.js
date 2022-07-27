5bf567f4ec379425fe8d38a1546d1d5bbeb9144d
Basant Mohamed <39272090+basantmohamed3@users.noreply.github.com>
2022-07-27 08:22:19 +0200

    Time: 75 ms (70.19%), Space: 42.2 MB (31.42%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -6,14 +6,7 @@ var lengthOfLastWord = function(s) {
     let strArr =s.trim().split(" ")
     let count =0
     
-let lastWord = strArr[strArr.length-1]
-for(var i =0; i < lastWord.length; i++){
-    count= count+1
-
-
+let lastWord = strArr[strArr.length-1].length
+return lastWord
 
 }
-    return count
-  
-    
-};
